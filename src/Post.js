22e1bbf4949b7ef115b9419196df965299025b67
Devan Beitel <devan@devanb.us>
2018-03-15 16:08:19 -0400

    Migrate to Mutation component.

--- src/Post.js
+++ src/Post.js
@@ -4,54 +4,50 @@ import ALL_POSTS_QUERY from "./graphql/feed.graphql";
 import DELETE_MUTATION from "./graphql/deletePost.graphql";
 import TOGGLE_LIKED_POST from "./graphql/toggleLikedPost.graphql";
 
-class Post extends React.Component {
-  render() {
-    const { imageUrl, id, description, liked } = this.props;
-    console.log(id);
-    return (
-      <div className="pa3 bg-black-05 ma3">
-        <div
-          className="w-100"
-          style={{
-            backgroundImage: `url(${imageUrl})`,
-            backgroundSize: "cover",
-            paddingBottom: "100%"
-          }}
-        />
-        <div className="pt3">
-          {description}{" "}
-          <span className="red f6 pointer dim" onClick={this.handleDelete}>
-            Delete
+const handleUpdate = (store, { data: { deletePost } }) => {
+  const data = store.readQuery({ query: ALL_POSTS_QUERY });
+  const newPosts = data.feed.filter(post => post.id !== deletePost.id);
+  store.writeQuery({
+    query: ALL_POSTS_QUERY,
+    data: {
+      ...data,
+      feed: newPosts
+    }
+  });
+};
+
+const Post = ({ description, id, imageUrl, liked }) => (
+  <div className="pa3 bg-black-05 ma3">
+    <div
+      className="w-100"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+        backgroundSize: "cover",
+        paddingBottom: "100%"
+      }}
+    />
+    <Mutation
+      mutation={DELETE_MUTATION}
+      variables={{ id: id }}
+      update={handleUpdate}
+    >
+      {handleDelete => (
+        <span className="red f6 pointer dim" onClick={handleDelete}>
+          Delete
+        </span>
+      )}
+    </Mutation>
+    {liked ? (
+      "Liked!"
+    ) : (
+      <Mutation mutation={TOGGLE_LIKED_POST} variables={{ id: id }}>
+        {toggleLike => (
+          <span>
+            <button onClick={toggleLike}>Like</button>
           </span>
-        </div>
-        <Mutation mutation={TOGGLE_LIKED_POST} variables={{ id: id }}>
-          {toggleLike => (
-            <span>
-              <button onClick={toggleLike}>Like</button>
-            </span>
-          )}
-        </Mutation>
-        {liked ? "Liked!" : "Not Liked :("}
-      </div>
-    );
-  }
-
-  handleDelete = () => {
-    this.props.deletePost({
-      variables: { id: this.props.id },
-      update: (store, { data: { deletePost } }) => {
-        const data = store.readQuery({ query: ALL_POSTS_QUERY });
-        const newPosts = data.feed.filter(post => post.id !== deletePost.id);
-        store.writeQuery({
-          query: ALL_POSTS_QUERY,
-          data: {
-            ...data,
-            feed: newPosts
-          }
-        });
-      }
-    });
-  };
-}
-
-export default graphql(DELETE_MUTATION, { name: "deletePost" })(Post);
+        )}
+      </Mutation>
+    )}
+  </div>
+);
+export default Post;
